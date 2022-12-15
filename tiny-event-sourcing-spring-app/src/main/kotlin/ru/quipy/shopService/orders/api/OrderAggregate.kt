2b905ad6e2c62832f7a506cb2c59f0b721d95a49
lev <leva22.08.01@inbox.ru>
2022-12-15 23:07:22 +0300

    feat: second version of shop

--- tiny-event-sourcing-spring-app/src/main/kotlin/ru/quipy/shopService/orders/api/OrderAggregate.kt
+++
@@ -1,7 +0,0 @@
-package ru.quipy.shopService.orders.api
-
-import ru.quipy.core.annotations.AggregateType
-import ru.quipy.domain.Aggregate
-
-@AggregateType(aggregateEventsTableName = "orders")
-class OrderAggregate: Aggregate
