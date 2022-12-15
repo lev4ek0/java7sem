084ca8478b965315da9014287fae7cc6e278ca17
lev <leva22.08.01@inbox.ru>
2022-12-15 23:05:35 +0300

    feat: second version of shop

--- tiny-event-sourcing-spring-app/src/test/kotlin/ru.quipy/BookProductTest.kt
+++ tiny-event-sourcing-spring-app/src/test/kotlin/ru.quipy/BookProductTest.kt
@@ -1,6 +1,5 @@
 package ru.quipy
 
-import org.awaitility.kotlin.await
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,20 +9,21 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import ru.quipy.core.EventSourcingService
-import ru.quipy.shopService.orders.api.OrderAggregate
-import ru.quipy.shopService.orders.logic.Order
-import ru.quipy.shopService.warehouse.api.WarehouseAggregate
-import ru.quipy.shopService.warehouse.logic.Warehouse
+import ru.quipy.shopService2.warehouse.api.WarehouseAggregate
+import ru.quipy.shopService2.warehouse.logic.Warehouse
+import java.lang.Exception
 import java.math.BigDecimal
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 @SpringBootTest
 class BookProductTest {
     companion object {
         private val orderId = UUID.randomUUID()
+        private val orderId2 = UUID.randomUUID()
+        private val orderId3 = UUID.randomUUID()
+        private val orderId4 = UUID.randomUUID()
         private val productId = UUID.randomUUID()
         private val warehouseId = UUID.randomUUID()
         private val amount = BigDecimal(9)
@@ -31,9 +31,6 @@ class BookProductTest {
         private val price = BigDecimal(100)
         private val title = "Iphone 14 pro"
     }
-
-    @Autowired
-    private lateinit var orderEsService: EventSourcingService<UUID, OrderAggregate, Order>
 
     @Autowired
     private lateinit var warehouseEsService: EventSourcingService<UUID, WarehouseAggregate, Warehouse>
@@ -55,17 +52,81 @@ class BookProductTest {
 
 
     @Test
+    fun createWarehouse() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
+        }
+
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertEquals(state.getId(), warehouseId)
+    }
+
+    @Test
+    fun addProduct() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.addProductToWarehouse(id = productId, title = title, price = price, amount = amount)
+        }
+
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertNotNull(state.products[productId])
+        Assertions.assertEquals(state.products[productId]!!.amount, amount)
+    }
+
+    @Test
+    fun createOrder() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.createNewOrder(id = orderId)
+        }
+
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertNotNull(state.orders[orderId])
+    }
+
+    @Test
     fun addProductToOrder() {
         warehouseEsService.create {
             it.createNewWarehouse(id = warehouseId)
         }
 
         warehouseEsService.update(warehouseId) {
-            it.createNewProduct(id = productId, title = title, price = price, amount = amount)
+            it.addProductToWarehouse(id = productId, title = title, price = price, amount = amount)
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.createNewOrder(id = orderId)
         }
 
 
-        orderEsService.create {
+        warehouseEsService.update(warehouseId) {
+            it.bookProduct(productId = productId, orderId = orderId, amount = BigDecimal(3))
+        }
+
+
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertEquals(state.orders[orderId]!!.products[productId], BigDecimal(3))
+        Assertions.assertEquals(state.products[productId]!!.amount, BigDecimal(6))
+        Assertions.assertEquals(state.products[productId]!!.booked_amount, BigDecimal(3))
+    }
+
+    @Test
+    fun addProductToOrderThreading() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.addProductToWarehouse(id = productId, title = title, price = price, amount = amount)
+        }
+
+        warehouseEsService.update(warehouseId) {
             it.createNewOrder(id = orderId)
         }
 
@@ -74,25 +135,128 @@ class BookProductTest {
         val latch = CountDownLatch(numberOfThreads)
         for (i in 0 until numberOfThreads) {
             service.submit {
-                orderEsService.update(orderId) {
-                    it.addProductToOrder(warehouseId = warehouseId, productId = productId, amount = to_book)
+                try {
+                    warehouseEsService.update(warehouseId) {
+                        it.bookProduct(productId = productId, orderId = orderId, amount = BigDecimal(3))
+                    }
+                } catch (_: Exception) {
+
                 }
                 latch.countDown()
             }
         }
         latch.await()
 
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertEquals(state.orders[orderId]!!.products[productId], BigDecimal(9))
+        Assertions.assertEquals(state.products[productId]!!.amount, BigDecimal.ZERO)
+        Assertions.assertEquals(state.products[productId]!!.booked_amount, BigDecimal(9))
+    }
 
-        await.atMost(10, TimeUnit.MINUTES).until {
-            warehouseEsService.getState(warehouseId)!!.products[productId]!!.amount == BigDecimal.ZERO
+    @Test
+    fun addProductToOrders() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
         }
+
+        warehouseEsService.update(warehouseId) {
+            it.addProductToWarehouse(id = productId, title = title, price = price, amount = BigDecimal(2698))
+        }
+
+
+//        val orderIds: Array<UUID> = arrayOf(orderId, orderId2, orderId3, orderId4)
+        val orderIds: ArrayList<UUID> = arrayListOf()
+
+        for(n in 1..900){
+            val uuid = UUID.randomUUID()
+            orderIds.add(uuid)
+            warehouseEsService.update(warehouseId) {
+                it.createNewOrder(id = uuid)
+            }
+
+        }
+
+        val factIds: ArrayList<UUID> = arrayListOf()
+        val numberOfThreads = 30
+        val service = Executors.newFixedThreadPool(30)
+        val latch = CountDownLatch(numberOfThreads)
+        for (i in 0 until numberOfThreads) {
+            service.submit {
+                for (n in 0..29) {
+                    try {
+                        warehouseEsService.update(warehouseId) {
+                            it.bookProduct(productId = productId, orderId = orderIds[i * 30 + n], amount = BigDecimal(3))
+                        }
+                    } catch (_: Exception) {
+
+                    } finally {
+                        factIds.add(orderIds[i * 30 + n])
+                    }
+
+                }
+
+                latch.countDown()
+            }
+        }
+        latch.await()
+        println(factIds.size)
+        val state = warehouseEsService.getState(warehouseId)!!
+        Assertions.assertEquals(state.orders[factIds[0]]!!.products[productId], BigDecimal(3))
+        Assertions.assertEquals(state.orders[factIds[1]]!!.products[productId], BigDecimal(3))
+        Assertions.assertEquals(state.orders[factIds[2]]!!.products[productId], BigDecimal(3))
+//        Assertions.assertEquals(state.products[productId]!!.booked_amount, BigDecimal(2697))
+        val bookedAmount = state.products[productId]!!.booked_amount
+        val nullAmount = (BigDecimal(2700) - bookedAmount) / BigDecimal(3)
+        println(bookedAmount)
+        var inc = 0
+        for (i in 0..899) {
+            if (state.orders[factIds[i]]!!.products[productId] == null) {
+                inc++
+            }
+        }
+        Assertions.assertEquals(nullAmount, BigDecimal(inc))
+        Assertions.assertNull(state.orders[factIds[899]]!!.products[productId])
+    }
+
+    @Test
+    fun addRandomAmountOfProductsToOrderThreading() {
+        warehouseEsService.create {
+            it.createNewWarehouse(id = warehouseId)
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.addProductToWarehouse(id = productId, title = title, price = price, amount = BigDecimal(250))
+        }
+
+        warehouseEsService.update(warehouseId) {
+            it.createNewOrder(id = orderId)
+        }
+
+        val numberOfThreads = 100
+        val service = Executors.newFixedThreadPool(50)
+        val latch = CountDownLatch(numberOfThreads)
+        var total = 0
+        for (i in 0 until numberOfThreads) {
+            service.submit {
+                val rand = (1..10).random()
+                try {
+                    warehouseEsService.update(warehouseId) {
+                        it.bookProduct(productId = productId, orderId = orderId, amount = BigDecimal(rand))
+                    }
+                } catch (_: Exception) {
+                    total -= rand
+                } finally {
+                    total += rand
+                }
+                latch.countDown()
+            }
+        }
+        latch.await()
 
         val state = warehouseEsService.getState(warehouseId)!!
-
-        Assertions.assertEquals(state.products[productId]!!.booked_amount, amount)
-
-        await.atMost(10, TimeUnit.MINUTES).until {
-            orderEsService.getState(orderId)!!.products[productId] == amount
-        }
+        println(total)
+        Assertions.assertEquals(state.orders[orderId]!!.products[productId], BigDecimal(total))
+        Assertions.assertEquals(state.products[productId]!!.amount, BigDecimal(250 - total))
+        Assertions.assertEquals(state.products[productId]!!.booked_amount, BigDecimal(total))
     }
 }
